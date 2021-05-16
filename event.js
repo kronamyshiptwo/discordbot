0570fee215ab8757b462152724b6f6052ff7aa28
kronamyshiptwo <84277534+kronamyshiptwo@users.noreply.github.com>
2021-05-16 10:44:57 -0400

    add trigger

--- event.js
+++ event.js
@@ -3,6 +3,13 @@ const client = new discord.Client()
 var login = function(token){
 client.login(token)
 }
+var trigger = function(command, reply){
+client.on("message", message => {
+if(message.content === command){
+message.channel.send(reply)
+}
+})
+}
 module.exports{
 login: login
 }
